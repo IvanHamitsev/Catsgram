42ba59ab605c4ad1de5f6448d1b76d339e77fc8a
Ivan <hamitsev-ie@yandex.ru>
2024-05-10 22:26:46 +0300

    Доработка в рамках темы 6/7: REST и Spring Boot → Урок 6/7

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,92 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        // проверяем, что email хотябы заполнен
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Имейл должен быть указан");
+        }
+
+        // проверяем, что такой email ещё не используется
+        if (getUserByEmail(user.getEmail()).isPresent()) {
+            throw new DuplicatedDataException("Этот имейл уже используется");
+        }
+
+        // заполняем данные пользователя
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        if (user.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+
+        if (users.containsKey(user.getId())) {
+            User userInDatabase = users.get(user.getId());
+            Optional<User> anotherUser = getUserByEmail(user.getEmail());
+            if (anotherUser.isPresent() && anotherUser.get().getId() != user.getId()) {
+                throw new DuplicatedDataException("Этот имейл уже используется");
+            }
+
+            if (user.getEmail() != null && !user.getEmail().isBlank()) {
+                userInDatabase.setEmail(user.getEmail());
+            }
+
+            if (user.getUsername() != null && !user.getUsername().isBlank()) {
+                userInDatabase.setUsername(user.getUsername());
+            }
+
+            if (user.getPassword() != null && !user.getPassword().isBlank()) {
+                userInDatabase.setPassword(user.getPassword());
+            }
+
+            return userInDatabase;
+        }
+        throw new NotFoundException("Пользователь с id = " + user.getId() + " не найден");
+    }
+
+    // вспомогательный метод для генерации идентификатора нового пользователя
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    // вспомогательный метод получения пользователя по email
+    private Optional<User> getUserByEmail(String email) {
+        return users.values()
+                .stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst();
+    }
+}
